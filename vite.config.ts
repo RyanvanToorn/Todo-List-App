50e5e15c9561ebf1d200c609ecc98e050cf87b72
Ryan van Toorn <156212766+RyanvanToorn@users.noreply.github.com>
2026-06-22 17:16:35 +0200

    feat: added aliases for types, interfaces and components folders

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, 'src/components'),
+      '@interfaces': resolve(__dirname, 'src/interfaces'),
+      '@types': resolve(__dirname, 'src/types'),
+    },
+  },
 })
